6bbc16b1df9c791d88d39adeff93201149175218
2025815-ui <2025815@student.cct.ie>
2026-05-08 22:12:08 +0100

    Initial Commit - Creating Project file

---
+++ src/main/java/com/mycompany/java/bank/system/JavaBankSystem.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.java.bank.system;
+
+/**
+ *
+ * @author yarzarmin
+ */
+public class JavaBankSystem {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
