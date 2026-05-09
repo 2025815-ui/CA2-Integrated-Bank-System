22e4785df4c04fbbac860486d1a7a5526171ddb3
2025815-ui <2025815@student.cct.ie>
2026-05-09 18:51:26 +0100

    Create FileReader class in correct path

---
+++ src/main/java/com/mycompany/java/bank/system/ApplicantFileReader.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.java.bank.system;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
+/**
+ *
+ * @author yarzarmin
+ */
+public class ApplicantFileReader {
+    
+}
