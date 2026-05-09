1f7608aca415b400d27ac65e3230609171a907de
2025815-ui <2025815@student.cct.ie>
2026-05-09 18:57:14 +0100

    updating FileReader class code

--- src/main/java/com/mycompany/java/bank/system/ApplicantFileReader.java
+++ src/main/java/com/mycompany/java/bank/system/ApplicantFileReader.java
@@ -13,4 +13,23 @@ import java.util.ArrayList;
  */
 public class ApplicantFileReader {
     
+    public static ArrayList<String> readNames(String filename){
+        ArrayList<String> names = new ArrayList<>();
+        
+        try{
+        BufferedReader reader = new BufferedReader(new FileReader(filename));
+            String line;
+            while((line = reader.readLine()) != null){
+                if(!line.trim().isEmpty()){
+                    names.add(line);
+                }
+            }
+            reader.close();
+            System.out.println("File read successfully.");
+            
+        }catch(Exception e){
+            System.out.println("File not found : " + filename);
+        }
+        return names;
+    }
 }
