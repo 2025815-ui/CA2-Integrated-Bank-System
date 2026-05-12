714db597b86c413bb82cce3133e9c912ab0f8e5d
2025815-ui <2025815@student.cct.ie>
2026-05-12 16:13:24 +0100

    Creating searching algorithm using binary search

--- src/main/java/com/mycompany/java/bank/system/EmployeeSearcher.java
+++ src/main/java/com/mycompany/java/bank/system/EmployeeSearcher.java
@@ -9,34 +9,42 @@ import java.util.ArrayList;
  *
  * @author yarzarmin
  */
+//Searching using Binary Search Algorithms
 public class EmployeeSearcher {
     
     public static Employee binarySearch(ArrayList<Employee> employee, String targetName){
         
+        //start with full array
         int left = 0;
         int right = employee.size()-1;
         
+        //keep searching until valid range 
         while(left <= right){
             
+            //finding middle position
             int middle = (left + right)/2;
             
+            //keeping middle in current
             String currentName = employee.get(middle).getemployeeName();
             
+            //compare middle with the searching name
             int comparison = currentName.compareToIgnoreCase(targetName);
             
-            
+            //Name found
             if(comparison == 0){
                 return employee.get(middle);
             }
-            
+            //current comes before targetname
             if(comparison < 0){
                 
                 left = middle +1;
+             //current comes after targetname   
             }else{
                 
                 left = middle -1;
             }
         }
-        return null;
+        
+        return null;//name not found in the list
     }
 }
